9eaaadc7f225f5c0bd731419b5a97075afa04a63
Liliana Everett <everettliliana@gmail.com>
2019-09-04 15:03:54 +0100

    mvp done

---
+++ lab_enumeration_callbacks_films_start/models/cinema.js
@@ -0,0 +1,47 @@
+const Cinema = function (films) {
+  this.films = films;
+};
+
+Cinema.prototype.getAllFilmTitles = function(){
+  const filmTitles = this.films.map (function(film) {
+    return film.title
+  })
+  return filmTitles
+}
+
+Cinema.prototype.findByTitle = function(title) {
+  const foundFilm = this.films.find(function(film){
+    return film.title === title
+  })
+  return foundFilm
+}
+
+Cinema.prototype.findByGenre = function(genre) {
+  const foundFilms = this.films.filter(function(film){
+    return film.genre === genre
+  })
+  return foundFilms
+}
+
+Cinema.prototype.checkFilmsOfYear = function(year) {
+  return this.films.some(function(film){
+    return film.year === year
+  })
+}
+
+Cinema.prototype.checkAllFilmsOverLength = function(length) {
+
+  return this.films.every(function(film){
+    return film.length > length
+  })
+}
+
+Cinema.prototype.totalRunningTime = function(){
+  const total = this.films.reduce(function(accumulator, film) {
+    return accumulator + film.length
+  }, 0)
+
+  return total
+}
+
+module.exports = Cinema;
---
+++ lab_enumeration_callbacks_films_start/specs/cinema_spec.js
@@ -0,0 +1,73 @@
+const assert = require('assert');
+const Cinema = require('../models/cinema.js');
+const Film = require('../models/film.js');
+
+describe('Cinema', function () {
+
+  let moonlight;
+  let bladeRunner;
+  let dunkirk;
+  let blackPanther;
+  let trainspotting;
+  let films;
+  let cinema;
+
+  beforeEach(function () {
+    moonlight = new Film('Moonlight', 'drama', 2016, 111);
+    bladeRunner = new Film('Blade Runner 2049', 'sci-fi', 2017, 164);
+    dunkirk = new Film('Dunkirk', 'history', 2017, 96);
+    blackPanther = new Film('Black Panther', 'action', 2018, 134);
+    trainspotting = new Film('T2 Trainspotting', 'drama', 2017, 117);
+
+    films = [moonlight, bladeRunner, dunkirk, blackPanther, trainspotting];
+    cinema = new Cinema(films);
+  });
+
+  it('should have a collection of films', function () {
+    const actual = cinema.films;
+    assert.deepStrictEqual(actual, films);
+  });
+
+  it('should be able to get a list of film titles', function () {
+    actual = cinema.getAllFilmTitles()
+    expected = ['Moonlight', 'Blade Runner 2049', 'Dunkirk', 'Black Panther', 'T2 Trainspotting']
+    assert.deepStrictEqual(actual, expected)  
+  });
+
+  it('should be able to find a film by title', function(){
+    actual = cinema.findByTitle('Dunkirk')
+    assert.deepStrictEqual(actual, dunkirk)
+  });
+
+  it('should be able to filter films by genre', function(){
+    actual = cinema.findByGenre('drama')
+    expected = [moonlight, trainspotting]
+    assert.deepStrictEqual(actual, expected)
+  });
+
+  it('should be able to check whether there are some films from a particular year', function(){
+    actual = cinema.checkFilmsOfYear(2017)
+    assert.strictEqual(actual, true)
+  });
+
+  it('should be able to check whether there are no films from a particular year', function(){
+    actual = cinema.checkFilmsOfYear(2020)
+    assert.strictEqual(actual, false)
+  });
+
+  it('should be able to check whether all films are over a particular length', function(){
+    actual = cinema.checkAllFilmsOverLength(80)
+    assert.strictEqual(actual, true)
+  });
+
+  it('should be able to check whether all films are over a particular length', function(){
+    actual = cinema.checkAllFilmsOverLength(100)
+    assert.strictEqual(actual, false)
+  });
+
+  it('should be able to calculate total running time of all films', function(){
+    actual = cinema.totalRunningTime()
+    assert.strictEqual(actual, 622)
+  });
+
+});
